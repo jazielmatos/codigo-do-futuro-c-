48a7397de06a1fe7d24ccb20be523ddec019eef0
jazielmatos <jazielmatos82@gmail.com>
2022-12-12 18:32:17 -0300

    Comitado

---
+++ exercicio1/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    internal class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int qtd { get; set; }
+
+        public static int qtdID = 0;
+
+        public Produto(int id,string nome,int qtd)
+        {
+            qtdID ++;
+            this.Id = id + qtdID;
+            this.Nome = nome;
+            this.qtd = qtd;
+
+        }
+        
+    }
+}
---
+++ exercicio1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha umas das opções abaixo: ");
+                Console.WriteLine("1 - Cadastrar Produto");
+                Console.WriteLine("2 - Listar Produtos");
+                Console.WriteLine("3 - Listar Qtd de Produtos Total no Estoque");
+                Console.WriteLine("4 - Sair");
+                int opcao = 0;
+                int.TryParse(Console.ReadLine(), out opcao);
+                switch (opcao)
+                {
+                    case 1:
+                        criarProduto(produtos);
+                        break;
+
+                    case 2:
+                        listarProduto(produtos);
+                        break;
+
+                    case 3:
+                        calcularQtdProdutos(produtos);
+                        break;
+
+                    case 4:
+                        return;
+                    
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Opção não existe!");
+                        Thread.Sleep(1300);
+                       break;
+                }
+                
+                
+            }
+        }
+
+        private static void criarProduto(List<Produto> produtos)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o nome do Produto");
+            string nomeProduto = Console.ReadLine().ToUpper().Trim();
+            Console.WriteLine("Digite a quantidade de " + nomeProduto);
+            int qtd = 0;
+            int.TryParse(Console.ReadLine(), out qtd);
+            Produto produto = new Produto(0, nomeProduto, qtd);
+            produtos.Add(produto);
+            Console.Clear();
+            Console.WriteLine($"Produto [ {produto.Nome} ], cadastrado com sucesso!");
+            Thread.Sleep(1500);    
+        }
+        private static void listarProduto(List<Produto> produtos)
+        {
+            Console.Clear();
+            if(produtos.Count != 0)
+            {
+                Console.WriteLine("===========================");
+                foreach (Produto produto in produtos)
+                {
+                    Console.WriteLine("\nID........: " + produto.Id);
+                    Console.WriteLine("Nome......: " + produto.Nome);
+                    Console.WriteLine("Quantidade: " + produto.qtd+"\n");
+                }
+                Console.WriteLine("===========================");
+                Console.WriteLine("\nAperte ENTER para voltar");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Não há produtos cadastrados");
+                Thread.Sleep(1000);
+            }
+
+        }
+
+        private static void calcularQtdProdutos(List<Produto> produtos)
+        {
+            Console.Clear();
+            int qtdTotal = 0;
+            foreach(Produto produto in produtos)
+            {
+                qtdTotal += produto.qtd;
+            }
+            Console.WriteLine("=========================================================");
+            Console.WriteLine("A quantidade de produtos total no estoque é: "+qtdTotal);
+            Console.WriteLine("=========================================================");
+            Console.WriteLine("\nAperte ENTER para voltar");
+            Console.ReadLine();
+        }
+    }
+}
